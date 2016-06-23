705d04692111d0fdf941e362e82d97e2ecb91504
GGQ-X201T <gqguan@hotmail.com>
2016-06-23 13:14:39 +0800

    Set the face index (i_face) with the macro of C_FACE. However, the C_FACE needs an additional argument for the local face index, which is usually produced in the loop of c_face_loop.

--- specX.c
+++ specX.c
@@ -16,15 +16,17 @@ DEFINE_SOURCE(evap_adj_membr, i_cell, t_cell, dS, eqn)
 {
 	Domain *domain; 
 	Thread *t_FeedInterface; // pointer of the thread of faces
-	face_t i_face = -1; 
+	face_t i_face = -1; // index of face
 	cell_t i_cell0, i_cell1 = -1; // indexes of adjacent cells for boundary identification
-	real source;
+	int i_local;
+	real source; // returning result
 
 	domain = Get_Domain(1); // explicit declaration of mixture
 	t_FeedInterface = Lookup_Thread(domain, 13); // explicit get the boundary, id of 13 should be consisted with GUI display
-
+	
 	if (THREAD_ID(t_cell) == 5) // check the zone, id of 5 should be shown at GUI display
 	{
+		i_face = C_FACE(i_cell, t_cell, i_local); // global face index for the given cell
 		i_cell0 = F_C0(i_face, t_FeedInterface); // return the cell index of adjacent face, ref. "3.2.5 connectivity macro"
 		i_cell1 = F_C1(i_face, t_FeedInterface); // return none at the boundary
 		if (i_cell1 == -1)
