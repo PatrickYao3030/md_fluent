53e4288349ce50fa57a88b3ca5c5604b52b3d9b8
GGQ-X201T <gqguan@hotmail.com>
2016-06-25 01:26:09 +0800

    Loop the faces for the given cell (i_cell, t_cell)

--- specX.c
+++ specX.c
@@ -22,20 +22,23 @@ DEFINE_SOURCE(evap_adj_membr, i_cell, t_cell, dS, eqn)
 	real source; // returning result
 
 	domain = Get_Domain(1); // explicit declaration of mixture
-	t_FeedInterface = Lookup_Thread(domain, 13); // explicit get the boundary, id of 13 should be consisted with GUI display
+	t_FeedInterface = Lookup_Thread(domain, 13); // explicitly get the boundary, whose id (13) should be consisted with GUI display
 	
-	if (THREAD_ID(t_cell) == 5) // check the zone, id of 5 should be shown at GUI display
+	if (THREAD_ID(t_cell) == 5) // check the zone, whose id (5) should be shown at GUI display
 	{
-		i_face = C_FACE(i_cell, t_cell, i_local); // global face index for the given cell
-		i_cell0 = F_C0(i_face, t_FeedInterface); // return the cell index of adjacent face, ref. "3.2.5 connectivity macro"
-		i_cell1 = F_C1(i_face, t_FeedInterface); // return none at the boundary
-		if (i_cell1 == -1)
+		c_face_loop(i_cell, t_cell, i_local) // loop all faces for the given cell of (i_cell, t_cell)
 		{
-			source = 0.01;
-		}
-		else
-		{
-			source = 0.;
+			i_face = C_FACE(i_cell, t_cell, i_local); // global face index for the given cell
+			i_cell0 = F_C0(i_face, t_FeedInterface); // return the adjacent cell index of the face, ref. "3.2.5 connectivity macro"
+			i_cell1 = F_C1(i_face, t_FeedInterface); // return none at the boundary
+			if (i_cell1 == -1)
+			{
+				source = 0.01;
+			}
+			else
+			{
+				source = 0.;
+			}
 		}
 	}
   dS[eqn] = 0.;
