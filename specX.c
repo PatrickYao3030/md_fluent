a22fc34c63af30da060621922f555f6f40ccadec
GGQ-X201T <gqguan@hotmail.com>
2016-07-05 10:43:28 +0800

    identify the cells adhered to the feed-side membrane wall, and set corresponding UDMI(0) as -1; similarly, the cells which are adjacent to the permeate-side membrane wall are set as +1

--- specX.c
+++ specX.c
@@ -19,14 +19,16 @@ DEFINE_INIT(idf_cells, domain)
  	cell_t i_cell0, i_cell1; // global cell index
 	cell_t i_cell;
 	face_t i_face = -1; // global face index
-	Thread *t_FeedFluid;
-	Thread *t_FeedInterface;
+	Thread *t_FeedFluid, *t_PermFluid;
+	Thread *t_FeedInterface, *t_PermInterface;
 	Thread *t_cell;
 	real loc0[ND_ND], loc1[ND_ND];
 	int i_local;
 
 	t_FeedFluid = Lookup_Thread(domain, 5);
+	t_PermFluid = Lookup_Thread(domain, 6);
 	t_FeedInterface = Lookup_Thread(domain, 13);
+	t_PermInterface = Lookup_Thread(domain, 14);
 	fout = fopen("idf_cells.out", "w");
 
 	thread_loop_c(t_cell, domain)
@@ -34,7 +36,7 @@ DEFINE_INIT(idf_cells, domain)
 		begin_c_loop(i_cell, t_cell)
 		{
 			C_CENTROID(loc0, i_cell, t_cell); // get the location of current cell
-			begin_f_loop(i_face, t_FeedInterface) // search the adjacent cells for the given boundary
+			begin_f_loop(i_face, t_FeedInterface) // search the adjacent cells for the feed-side membrane
 			{
 				i_cell0, i_cell1 = -1;
 				i_cell0 = F_C0(i_face, t_FeedInterface);
@@ -42,36 +44,32 @@ DEFINE_INIT(idf_cells, domain)
 				if (i_cell1 == -1)
 				{
 					C_CENTROID(loc1, i_cell0, t_FeedFluid); // get the location of cell centroid
-					if ((loc0[0] == loc1[0]) && (loc0[1] == loc1[1]))
+					if ((loc0[0] == loc1[0]) && (loc0[1] == loc1[1])) // do NOT use the sentence of ELSE because the cells will be evaluated repeatedly.
 					{
-						fprintf(fout, "Cell#%d %g %g\n", i_cell0, loc1[0], loc1[1]);
-						C_UDMI(i_cell, t_cell, 0) = 1;
+						//fprintf(fout, "Cell#%d %g %g\n", i_cell0, loc1[0], loc1[1]);
+						C_UDMI(i_cell, t_cell, 0) = -1;
 					}
 				}
 			}
 			end_f_loop(i_face, t_FeedInterface)
+			begin_f_loop(i_face, t_PermInterface) // search the adjacent cells for the permeate-side membrane
+			{
+				i_cell0, i_cell1 = -1;
+				i_cell0 = F_C0(i_face, t_PermInterface);
+				i_cell1 = F_C1(i_face, t_PermInterface);
+				if (i_cell1 == -1)
+				{
+					C_CENTROID(loc1, i_cell0, t_PermFluid);
+					if ((loc0[0] == loc1[0]) && (loc0[1] == loc1[1]))
+					{
+						C_UDMI(i_cell, t_cell, 0) = 1;
+					}
+				}
+			}
+			end_f_loop(i_face, t_PermInterface)
 		}
 		end_c_loop(i_cell, t_cell)
 	}
-
-	//begin_f_loop(i_face, t_FeedFluid)
-	//{
-	//	i_cell0, i_cell1 = -1; // initiate the global cell index
-	//	i_cell0 = F_C0(i_face, t_FeedInterface);
-	//	i_cell1 = F_C1(i_face, t_FeedInterface);
-	//	if (i_cell1 == -1)
-	//	{
-	//		//C_CENTROID(loc, i_cell0, t_FeedFluid); // get the location of cell centroid
-	//		F_UDMI(i_face, t_FeedFluid, 0) = 1; // use the UDMI0 to store the identified cell, where 1 means that the cell is adjacent to the boundary
-	//		//fprintf(fout, "Global cell#%d %g %g\n", i_cell0, loc[0], loc[1]);
-	//	}
-	//	else
-	//	{
-	//		F_UDMI(i_face, t_FeedFluid, 0) = -1; // use the UDMI0 to store the identified cell, where -1 indicates an interior cell
-	//	}
-	//}
-	//end_f_loop(i_face, t_FeedFluid)
-	//fprintf(fout, "Tranfer data are completed.\n");
 }
 
 DEFINE_SOURCE(evap_adj_membr, i_cell, t_cell, dS, eqn)
