8a1a94df94e2acfeccd1c625c5566f24b99e6285
gqguan <gqguan@hotmail.com>
2016-11-03 20:15:43 +0800

    Add ON_DEMAND "IdentifyInterCells"

--- src/specX.c
+++ src/specX.c
@@ -266,6 +266,89 @@ DEFINE_INIT(idf_cells_1007, domain)
 	fclose(fout1);
 }
 
+DEFINE_ON_DEMAND(IdentifyInterCells_1103)
+{
+	Domain *domain = Get_Domain(id_domain);
+	Thread *t_FeedFluid = Lookup_Thread(domain, id_FeedFluid);
+	Thread *t_PermFluid = Lookup_Thread(domain, id_PermFluid);
+	Thread *t_FeedInterface = Lookup_Thread(domain, id_FeedInterface);
+	Thread *t_PermInterface = Lookup_Thread(domain, id_PermInterface);
+	cell_t i_cell0, i_cell1;
+	face_t i_face0, i_face1;
+	Thread *t_cell;
+	real loc[ND_ND], loc0[ND_ND], loc1[ND_ND];
+	real InterfaceArea[ND_ND];
+	int i = 0;
+	real temp = 0.0;
+
+	fout0 = fopen("idf_cell0.out", "w");
+	fout1 = fopen("idf_cell1.out", "w");
+	gid = 0;
+
+	begin_f_loop(i_face0, t_FeedInterface) // find the adjacent cells for the feed-side membrane.
+	{
+		Message("1\n");
+		i_cell0 = F_C0(i_face0, t_FeedInterface); // get the index for the cell adjacent the 
+		C_CENTROID(loc0, i_cell0, t_FeedFluid); // get the location of cell centroid
+		C_UDMI(i_cell0, t_FeedFluid, 0) = -1; // mark the wall cells as -1, and others as 0 (no modification)
+		WallCell[gid][0].index = i_cell0; // output the cell info to the workspace variable WallCell
+		WallCell[gid][0].centroid[0] = loc0[0];
+		WallCell[gid][0].centroid[1] = loc0[1];
+		//WallCell[gid][0].temperature = C_T(i_cell0, t_FeedFluid); // because the flowing field has not initiated yet, no field info can be got
+		WallCell[gid][0].volume = C_VOLUME(i_cell0, t_FeedFluid);
+		F_AREA(InterfaceArea, i_face0, t_FeedInterface);
+		WallCell[gid][0].area = NV_MAG(InterfaceArea);
+		//WallCell[gid][0].massfraction.water = C_YI(i_cell0, t_FeedFluid, 0);
+		begin_f_loop(i_face1, t_PermInterface) // search the symmetric cell (THE LOOP CAN ONLY RUN IN SERIAL MODE)
+		{
+			i_cell1 = F_C0(i_face1, t_PermInterface);
+			C_CENTROID(loc1, i_cell1, t_PermFluid);
+			if (fabs(loc0[0]-loc1[0])/loc0[0] < EPS) // In this special case, the pair of wall cells on both sides of membrane are symmetrical
+			{
+				fprintf(fout0, "i_cell0-%d, %g, %g, i_cell1-%d, %g, %g\n", i_cell0, loc0[0], loc0[1], i_cell1, loc1[0], loc1[1]);
+				WallCell[gid][1].index = i_cell1;
+				WallCell[gid][1].centroid[0] = loc1[0];
+				WallCell[gid][1].centroid[1] = loc1[1];
+				//WallCell[gid][1].temperature = C_T(i_cell1, t_PermFluid);
+				WallCell[gid][1].volume = C_VOLUME(i_cell1, t_PermFluid);
+				F_AREA(InterfaceArea, i_face1, t_PermInterface);
+				WallCell[gid][1].area = NV_MAG(InterfaceArea);
+				//WallCell[gid][1].massfraction.water = C_YI(i_cell1, t_PermFluid, 0);
+			}
+		}
+		end_f_loop(i_face1, t_PermInterface)
+		gid++;
+	}
+	end_f_loop(i_face0, t_FeedInterface)
+
+	Message("The workspace WallCell[%d] has been created.\n", gid);
+	Message("The identified wall cells, by using the feed-side enumeration, are summarized in idf_cell0.out.\n");
+
+	begin_f_loop(i_face1, t_PermInterface) // find the adjacent cells for the permeate-side membrane.
+	{
+		i_cell1 = F_C0(i_face1, t_PermInterface);
+		C_CENTROID(loc1, i_cell1, t_PermFluid); // get the location of cell centroid
+		begin_f_loop(i_face0, t_FeedInterface)
+		{
+			i_cell0 = F_C0(i_face0, t_FeedInterface);
+			C_CENTROID(loc0, i_cell0, t_FeedFluid);
+			if (fabs(loc1[0]-loc0[0])/loc1[0] < EPS)
+			{
+				fprintf(fout1, "i_cell0-%d, %g, %g, i_cell1-%d, %g, %g\n", i_cell0, loc0[0], loc0[1], i_cell1, loc1[0], loc1[1]);
+			}
+		}
+		end_f_loop(i_face1, t_PermInterface)
+		C_UDMI(i_cell1, t_PermFluid, 0) = +1; // mark the cell
+	}
+	end_f_loop(i_face1, t_PermInterface)
+	
+	Message("The identified wall cells, by using the permeate-side enumeration, are summarized in redundant idf_cell1.out.\n");
+	Message("The idf_cell0.out should be same as idf_cell1.out.\n");
+
+	fclose(fout0);
+	fclose(fout1);
+}
+
 DEFINE_ON_DEMAND(InterCellID_0923)
 {
 	cell_t i_cell, i_cell0, i_cell1;
